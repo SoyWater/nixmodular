ec43a73eac63c24f6b065f685c85ac50929da813
SoyWater <ivanlxy123@gmail.com>
2026-05-02 16:34:13 +0800

    Update zen

--- wrapped-applications/programs.nix
+++ wrapped-applications/programs.nix
@@ -76,22 +76,15 @@
     };
     packages.zen-browser-wayland =
       let
-        zenFixed = inputs'.zen-browser.packages.default.overrideAttrs (old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [ ])
-            ++ [
-              pkgs.makeWrapper
-              pkgs.copyDesktopItems
-              pkgs.wrapGAppsHook3
-            ];
-        });
+        zenFixed = inputs'.zen-browser.packages.default;
+        zenMainProgram = zenFixed.meta.mainProgram or "zen";
       in
       pkgs.symlinkJoin {
         name = "zen-browser-wayland";
         paths = [ zenFixed ];
         nativeBuildInputs = [ pkgs.makeWrapper ];
         postBuild = ''
-          wrapProgram $out/bin/zen \
+          wrapProgram $out/bin/${zenMainProgram} \
             --set MOZ_ENABLE_WAYLAND 1
         '';
       };
