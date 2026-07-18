2d522e5b97cdd4d54c6be10debc24475e467b143
SoyWater <ivanlxy123@gmail.com>
2026-07-18 18:33:30 +0800

    Make Limine boot portable across firmware resets

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -21,8 +21,13 @@
 
         (lib.mkIf (config.bootLoader == "limine") {
           limine.enable = true;
-          limine.efiSupport = true;          
+          limine.efiSupport = true;
+          limine.efiInstallAsRemovable = true;
           limine.maxGenerations = 10;
+          limine.extraInstallCommands = ''
+            ${pkgs.coreutils}/bin/install -Dm700 ${config.boot.loader.efi.efiSysMountPoint}/EFI/BOOT/BOOTX64.EFI \
+              ${config.boot.loader.efi.efiSysMountPoint}/EFI/limine/BOOTX64.EFI
+          '';
 
           limine.secureBoot.enable = config.limine.secureBoot;
         })
