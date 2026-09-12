5a466177ae7ddc6172cca7bab1a3252ca0ec6805
SoyWater <ivanlxy123@gmail.com>
2026-09-12 15:36:59 +0800

    Add Nerd Font monospace fallbacks

--- modules/features/fonts.nix
+++ modules/features/fonts.nix
@@ -2,8 +2,16 @@
 {
   flake.nixosModules.fonts = { pkgs, ... }: {
     fonts = {
-      packages = [ pkgs.maple-mono.NF-CN ];
-      fontconfig.defaultFonts.monospace = [ "Maple Mono NF CN" ];
+      packages = [
+        pkgs.maple-mono.NF-CN
+        pkgs.nerd-fonts.fira-code
+        pkgs.nerd-fonts.jetbrains-mono
+      ];
+      fontconfig.defaultFonts.monospace = [
+        "Maple Mono NF CN"
+        "FiraCode Nerd Font Mono"
+        "JetBrainsMono Nerd Font Mono"
+      ];
     };
   };
 }
